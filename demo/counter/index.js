44ebc96ccf46b841ef0e0b5ae62dcec242ab260c
Aliez <Aliez@163.com>
2020-08-01 17:25:43 +0800

    Update: docs add demo

--- demo/counter/index.js
+++ demo/counter/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import {Card, Button} from 'antd';
+import {Card, Button, Input} from 'antd';
 import {Provider, useStore} from '../../src';
-import {store, add} from './store';
+import {store, mutations} from './store';
 import './index.css';
 import 'antd/dist/antd.css';
 
 function Count() {
   const countState = useStore((store) => {
+    const {state, computed} = store
+    const {plusOne, plusOneSquare} = computed
     return {
-      count: store.count,
+      count: state.count,
+      plusOne,
+      plusOneSquare
     }
   });
 
@@ -16,11 +20,50 @@ function Count() {
     <Card hoverable style={{marginBottom: 24}}>
       <h1>计数器</h1>
       <div className="chunk">
-        <div className="text-chunk">store中的count现在是 {countState.count}</div>
-        <Button onClick={add}>add</Button>
+        <div className="text-chunk">count: {countState.count}</div>
+        <div className="text-chunk">count加一: {countState.plusOne}</div>
+        <div className="text-chunk">count加一后平方: {countState.plusOneSquare}</div>
+        <Button onClick={mutations.add}>add</Button>
       </div>
     </Card>
   );
+}
+
+function Chat() {
+  const message = useStore((store) => store.state.message)
+
+  const _handleSend = msg => {
+    mutations.setMsg('')
+    mutations.sendMsg(msg)
+  }
+  return (
+    <Card hoverable style={{marginBottom: 24}}>
+      <h1>聊天室</h1>
+      <div className="text-chunk">当前消息是: {message}</div>
+      <Input.Search
+        placeholder="请输入消息"
+        enterButton="Send"
+        size="large"
+        value={message}
+        onChange={v => mutations.setMsg(v.target.value)}
+        onSearch={(msg) => _handleSend(msg)}
+      />
+    </Card>
+  );
+}
+
+function ViewArea() {
+  const msgList = useStore(store => store.state.msgList)
+  const _handleDelMsg = index => {
+    console.log('index', index)
+    mutations.delMsg(index)
+  }
+  return (
+    <Card hoverable style={{marginBottom: 24}}>
+      <h1>消息列表</h1>
+      {msgList.map((msg, i) => <div onClick={() => _handleDelMsg(i)} key={i}>{msg}</div>)}
+    </Card>
+  )
 }
 
 export default () => {
@@ -28,6 +71,8 @@ export default () => {
     <Provider value={store}>
       <div className="flex">
         <Count/>
+        <Chat/>
+        <ViewArea/>
       </div>
     </Provider>
   );
--- demo/counter/store.js
+++ demo/counter/store.js
@@ -1,16 +1,37 @@
-import {reactive, effect} from '@vue/reactivity';
+import {reactive, effect, computed} from '@vue/reactivity'
 
 export const state = reactive({
   count: 0,
-});
-
-export const add = () => {
-  state.count += 1
-};
-
-effect(() => {
-  console.log('count', state.count)
+  message: 'Hello',
+  msgList: []
 })
 
-export const store = reactive(state)
+const plusOne = computed(() => state.count + 1);
+const plusOneSquare = computed(() => plusOne.value * plusOne.value)
+
+const add = () => state.count += 1
+const setMsg = message => (state.message = message)
+const sendMsg = msg => (state.msgList = [...state.msgList, msg])
+const delMsg = index => {
+  console.log('msgList index', index)
+}
+
+effect(() => {
+  console.log('plusOne', plusOne)
+})
+
+export const mutations = {
+  add,
+  setMsg,
+  sendMsg,
+  delMsg
+}
+
+export const store = {
+  state,
+  computed: {
+    plusOne,
+    plusOneSquare
+  }
+}
 
